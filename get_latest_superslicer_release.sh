c550af043e17e76bdc337c8e710e63076758a730
tehniemer <jrniemand@gmail.com>
2025-03-27 11:51:51 -0500

    Update get_latest_superslicer_release.sh

--- get_latest_superslicer_release.sh
+++ get_latest_superslicer_release.sh
@@ -4,35 +4,42 @@ TMPDIR="$(mktemp -d)"
 
 curl -SsL https://api.github.com/repos/supermerill/SuperSlicer/releases/latest > $TMPDIR/latest.json
 
-# Grabs the first item of the latest result of the AppImage.
-url=$(jq -r '.assets[] | select(.browser_download_url|test("SuperSlicer-ubuntu_20.04-gtk2.*AppImage$"))|.browser_download_url' $TMPDIR/latest.json)
-name=$(jq -r '.assets[] | select(.browser_download_url|test("SuperSlicer-ubuntu_20.04-gtk2.*AppImage$"))|.name' $TMPDIR/latest.json)
-version=$(jq -r .tag_name $TMPDIR/latest.json)
+if [[ ! -e "$TMPDIR/latestReleaseInfo.json" ]]; then
 
-if [ $# -ne 1 ]; then
-  echo "Wrong number of params"
-  exit 1
-else
-  request=$1
+  curl -SsL https://api.github.com/repos/supermerill/SuperSlicer/releases/latest > $TMPDIR/latestReleaseInfo.json
+
 fi
 
-case $request in
+releaseInfo=$(cat $TMPDIR/latestReleaseInfo.json)
 
-  url)
-    echo $url
-    ;;
+if [[ $# -gt 1 ]]; then
 
-  name)
-    echo $name
-    ;;
+  VER=$2
 
-  version)
-    echo $version
-    ;;
+  if [[ ! -e "$TMPDIR/releases.json" ]]; then
+    curl -SsL https://api.github.com/repos/supermerill/SuperSlicer/releases > $TMPDIR/releases.json
+  fi
 
-  *)
-    echo "Unknown request"
-    ;;
-esac
+  allReleases=$(cat $TMPDIR/releases.json)
 
-exit 0
+fi
+
+if [[ "$1" == "url" ]]; then
+
+  echo "${releaseInfo}" | jq -r '.assets[] | .browser_download_url | select(test("SuperSlicer_.+(-\\w)?.linux64_(?!GTK3).+.tar.zip"))'
+
+elif [[ "$1" == "name" ]]; then
+
+  echo "${releaseInfo}" | jq -r '.assets[] | .name | select(test("SuperSlicer_.+(-\\w)?.linux64_(?!GTK3).+.tar.zip"))'
+
+elif [[ "$1" == "url_ver" ]]; then
+
+  # Note: Releases sometimes have hex-encoded ascii characters tacked on
+  # So version '2.0.0+' might need to be requested as '2.0.0%2B' since GitHub returns that as the download URL
+  echo "${allReleases}" | jq --arg VERSION "$VER" -r '.[] | .assets[] | .browser_download_url | select(test("SuperSlicer_" + $VERSION + "linux64_.+.tar.zip"))'
+
+elif [[ "$1" == "name_ver" ]]; then
+   
+  echo "${allReleases}" | jq --arg VERSION "$VER" -r '.[] | .assets[] | .name | select(test("SuperSlicer_" + $VERSION + "\\+linux64_.+.tar.zip"))'
+
+fi
