9e47eedfd3a5f55493601e3138b0c5a1264c258c
tehniemer <jrniemand@gmail.com>
2025-03-30 14:55:35 -0500

    Update get_latest_superslicer_release.sh

--- get_latest_superslicer_release.sh
+++ get_latest_superslicer_release.sh
@@ -5,8 +5,8 @@ TMPDIR="$(mktemp -d)"
 curl -SsL https://api.github.com/repos/supermerill/SuperSlicer/releases/latest > $TMPDIR/latest.json
 
 # Grabs the first item of the latest result of the AppImage.
-url=$(jq -r '.assets[] | select(.browser_download_url|test("SuperSlicer-ubuntu_.*?\\.*AppImage$"))|.browser_download_url' $TMPDIR/latest.json)
-name=$(jq -r '.assets[] | select(.browser_download_url|test("SuperSlicer-ubuntu_.*?\\.*AppImage$"))|.name' $TMPDIR/latest.json)
+url=$(jq -r '.assets[] | select(.browser_download_url|test("^(?!.*gtk[0-9]).*SuperSlicer-ubuntu_.*?\\.AppImage$"))|.browser_download_url' $TMPDIR/latest.json)
+name=$(jq -r '.assets[] | select(.browser_download_url|test("^(?!.*gtk[0-9]).*SuperSlicer-ubuntu_.*?\\.AppImage$"))|.name' $TMPDIR/latest.json)
 version=$(jq -r .tag_name $TMPDIR/latest.json)
 
 if [ $# -ne 1 ]; then
