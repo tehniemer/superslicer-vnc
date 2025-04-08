2968f5f495e789270c220b7c0d5d47b51ba086ed
tehniemer <jrniemand@gmail.com>
2025-04-08 14:59:24 +0000

    Update pre-release workflow logic

--- get_latest_superslicer_prerelease.sh
+++ get_latest_superslicer_prerelease.sh
@@ -4,8 +4,8 @@ TMPDIR="$(mktemp -d)"
 
 curl -SsL https://api.github.com/repos/supermerill/SuperSlicer/releases > $TMPDIR/prerelease.json
 
-# Filter the release that has both "target_commitish": "rc" and "prerelease": true
-release=$(jq -c '.[] | select(.target_commitish == "rc" and .prerelease == true)' $TMPDIR/prerelease.json)
+# Filter the release that has "prerelease": true
+release=$(jq -c '.[] | select(.prerelease == true)' $TMPDIR/prerelease.json)
 
 # Extract the first matching AppImage asset from the filtered release
 url=$(echo "$release" | jq -r '.assets[] | select(.browser_download_url|test("^(?!.*gtk[0-9]).*SuperSlicer-ubuntu_.*?\\.AppImage$")) | .browser_download_url' | head -n 1)
